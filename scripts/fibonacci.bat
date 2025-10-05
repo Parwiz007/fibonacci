1fa2f834b497b4a3de638068c01f039f5f975192
parwiz <parwiz007@gmail.com>
2025-10-04 23:32:30 -0400

    Update fibonacci.bat

--- scripts/fibonacci.bat
+++ scripts/fibonacci.bat
@@ -1,1 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
 
+:: Get input parameter, default to 10 if not provided
+set N=%1
+if "%N%"=="" set N=10
+
+set a=0
+set b=1
+
+echo The Fibonacci series is :
+
+for /l %%i in (0,1,%N%-1) do (
+    echo %%i    !a!
+    set /a fn=a+b
+    set /a a=b
+    set /a b=fn
+)
+
+endlocal
